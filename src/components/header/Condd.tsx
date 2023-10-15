dc94701d841254b98b123c1e615e0bc2be1584a0
yongmin <vmflxu@gmail.com>
2023-10-16 00:15:26 +0900

    TEST

---
+++ src/components/header/Condd.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+
+const Condd = ({ data }: { data: any }) => {
+    console.log(data);
+    return (
+        <div>Condd</div>
+    )
+}
+
+export default Condd
