27bc633a3b5eb89cc67b6de131bd99654ddb1c81
yongmin <vmflxu@gmail.com>
2023-10-29 18:51:19 +0900

    Fix: key 추가

--- src/app/article/[id]/_fragments/ArticleContent.tsx
+++ src/app/article/[id]/_fragments/ArticleContent.tsx
@@ -10,7 +10,7 @@ const ArticleContent = ({ content, tags }: ArticleContentProps) => {
     return (
         <Flex.VStack className={`w-[1200px] min-h-full h-fit text-${textColor.normal}`}>
             <div className='w-full min-h-[300px] h-fit mt-12 text-black' dangerouslySetInnerHTML={{ __html: content }}></div>
-            <Flex.HStack className='gap-4 mt-4 text-gray-400'>{tags.map(item => <span>{`#${item}`}</span>)}</Flex.HStack>
+            <Flex.HStack className='gap-4 mt-4 text-gray-400'>{tags.map((item, idx) => <span key={idx}>{`#${item}`}</span>)}</Flex.HStack>
             <div className='w-full h-[1px] bg-slate-700 my-4'></div>
         </Flex.VStack>
     );
