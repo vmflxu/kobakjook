5c92bc121dcccef2a561a710ade7c263d1e031a8
yongmin <vmflxu@gmail.com>
2023-11-08 18:55:28 +0900

    Fix: css 높이 정의 추가

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -19,7 +19,7 @@ const page = async ({ params, }: { params: PageParams, }) => {
     return (
         <Flex.HCenter className={'w-[1200px] h-fit flex-wrap gap-8 text-black'}>
             <SearchBox />
-            <div className='grid grid-flow-row grid-cols-4 gap-10 w-full'>
+            <div className='grid grid-flow-row grid-cols-4 gap-10 w-full h-fit'>
                 {/* <Flex.HCenter className='w-full flex-wrap gap-10'> */}
                 {(!!data && data.length > 0) && data.map((item, idx) => {
                     return <PostCard
