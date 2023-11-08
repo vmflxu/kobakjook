73d2ba2674899b2214a7a5cdf81a929f6a4ee3d6
yongmin <vmflxu@gmail.com>
2023-11-08 18:32:04 +0900

    Fix: 스피너 디자인변경

--- src/app/article/[id]/loading.tsx
+++ src/app/article/[id]/loading.tsx
@@ -1,11 +1,20 @@
 'use client'
 import { Flex } from '@/components/style/Flex'
 import React from 'react'
+import { ColorRing } from 'react-loader-spinner'
 
 const loading = () => {
   return (
     <Flex.Center className='w-full h-screen text-black text-3xl'>
-        데이터를 가져오는 중입니다.
+      <ColorRing
+        visible={true}
+        height="30%"
+        width="30%"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
     </Flex.Center>
   )
 }
