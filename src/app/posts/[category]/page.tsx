fa919069aac87a54ee32a3161d800921a9644cdd
yongmin <vmflxu@gmail.com>
2023-11-21 01:22:25 +0900

    Refactor: 코드 정리 및 스플릿

--- src/app/posts/[category]/page.tsx
+++ src/app/posts/[category]/page.tsx
@@ -1,11 +1,11 @@
-import { PostPayload } from '@/app/api/post/route';
 import { Flex } from '@/components/style/Flex';
 import { getHost } from '@/lib/getHost';
 import React from 'react'
-import PostCard from './_fragments/PostCard';
 import SearchBox from './_fragments/SearchBox';
 import { ResPostSchema } from '@/app/writepage/page';
 import LatestComments from './_fragments/LatestComments';
+import ArticleList from './_fragments/ArticleList';
+import { Line } from '@/components/common/Line';
 
 type PageParams = {
     category: string,
@@ -20,23 +20,10 @@ const page = async ({ params, }: { params: PageParams, }) => {
     return (
         <Flex.HCenter className={'w-full lg:w-[1200px] h-fit flex-wrap gap-8 text-black mb-24'}>
             <SearchBox data={data} />
-            <div className='w-full h-[1px] bg-gray-300' />
+            <Line.Helical color='gray' degree={300} />
             <LatestComments />
-            <div className='w-full h-[1px] bg-gray-300' />
-            <Flex.VCenter className='w-full gap-4'>
-                <div className='font-bold'>포스트</div>
-                <div className='grid grid-flow-dense grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 w-full h-fit'>
-                    {/* <Flex.HCenter className='w-full flex-wrap gap-10'> */}
-                    {(!!data && data.length > 0) && data.map((item, idx) => {
-                        return <PostCard
-                            key={idx}
-                            data={item}
-                        />
-                    })}
-                    {/* </Flex.HCenter> */}
-                </div>
-                {data.length === 0 && <Flex.Center className='h-screen'>{`No "${params.category}" Posts in Blog`}</Flex.Center>}
-            </Flex.VCenter>
+            <Line.Helical color='gray' degree={300} />
+            <ArticleList data={data} category={params.category} />
         </Flex.HCenter>
     )
 }
